b56fb75ad2b75c93c10cf9f5e7863eaa9a540e07
manasjadhav <manasjjadhav@gmail.com>
2022-12-21 18:55:01 +0530

    made a todo

--- database/db.js
+++ database/db.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql2");
+const { forEach } = require("./sqlQueries");
 const essentials = require("./sqlQueries");
 
 const connection = mysql.createConnection({
@@ -12,13 +13,9 @@ connection.connect((err) => {
   if (err) throw err;
   console.log("Connected to SQL database...");
 
-  connection.query(essentials[0]);
-  connection.query(essentials[1]);
-  connection.query(essentials[2]);
-  connection.query(essentials[3]);
-  connection.query(essentials[4]);
-  connection.query(essentials[5]);
-  connection.query(essentials[6]);
+  essentials.forEach((item) => {
+    connection.query(item);
+  });
 });
 
 module.exports = connection;
