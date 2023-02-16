6aacdfb305544729a6b465fef2ac7acb65202405
Rohit Patil <rohitvpatil0810@gmail.com>
2023-02-17 00:07:26 +0530

    Added project table in database

--- database/sqlQueries.js
+++ database/sqlQueries.js
@@ -3,19 +3,19 @@ let sqlQuery2 =
   "CREATE TABLE IF NOT EXISTS admin (id VARCHAR(255), name VARCHAR(255), email VARCHAR(255), mobile VARCHAR(255), password VARCHAR(255));";
 
 let sqlQuery3 =
-  "CREATE TABLE IF NOT EXISTS manager (managerId VARCHAR(255) PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), mobile VARCHAR(255), password VARCHAR(255));";
+  "CREATE TABLE IF NOT EXISTS manager (managerId VARCHAR(255) PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), mobile VARCHAR(255), password VARCHAR(255), active enum ('Active', 'Deleted') DEFAULT 'Active');";
 
 let sqlQuery4 =
-  "CREATE TABLE IF NOT EXISTS client (clientId VARCHAR(255) PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), mobile VARCHAR(255), organization VARCHAR(255), password VARCHAR(255));";
+  "CREATE TABLE IF NOT EXISTS client (clientId VARCHAR(255) PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), mobile VARCHAR(255), organization VARCHAR(255), password VARCHAR(255), active enum ('Active', 'Deleted') DEFAULT 'Active' );";
 
 let sqlQuery5 =
   "CREATE TABLE IF NOT EXISTS department (departmentId VARCHAR(255) PRIMARY KEY, departmentName VARCHAR(255), managerId VARCHAR(255), FOREIGN KEY (managerId) REFERENCES manager(managerId));";
 
 let sqlQuery6 =
-  "CREATE TABLE IF NOT EXISTS operator (operatorId VARCHAR(255) PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), mobile VARCHAR(255), password VARCHAR(255), departmentId VARCHAR(255), active enum('Active', 'Deleted'), FOREIGN KEY (departmentId) REFERENCES department(departmentId));";
+  "CREATE TABLE IF NOT EXISTS operator (operatorId VARCHAR(255) PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), mobile VARCHAR(255), password VARCHAR(255), departmentId VARCHAR(255), active enum ('Active', 'Deleted') DEFAULT 'Active', FOREIGN KEY (departmentId) REFERENCES department(departmentId));";
 
 let sqlQuery7 =
-  'CREATE TABLE IF NOT EXISTS task (taskID VARCHAR(255) PRIMARY KEY , clientId VARCHAR(255) , operatorId VARCHAR(255) , managerId VARCHAR(255) , ProjectName VARCHAR(255) , taskName VARCHAR(255) , taskDescription TEXT , openDate DATE , closeDate DATE , clientNote TEXT , managerNote TEXT , priority enum ("Low" , "Medium" , "High") , AssignationStatus enum ("Pending" , "Assigned" , "Reassigned") DEFAULT "Pending" , taskStatus enum ("Pending", "inProgress" , "Completed" , "Closed") DEFAULT "Pending" , clientApproval enum ("Pending" , "Accepted" , "Rejected") DEFAULT "Pending" , managerApproval enum ("Pending" , "Accepted" , "Rejected") DEFAULT "Pending" , taskCategory enum ("Scheduled" , "RunTime") , FOREIGN KEY (clientId) REFERENCES client(clientId) , FOREIGN KEY (operatorId) REFERENCES operator(operatorId) , FOREIGN KEY (managerId) REFERENCES manager(managerId));';
+  'CREATE TABLE IF NOT EXISTS task (taskID VARCHAR(255) PRIMARY KEY , clientId VARCHAR(255) , operatorId VARCHAR(255) , managerId VARCHAR(255) , projectId VARCHAR(255) , taskName VARCHAR(255) , taskDescription TEXT , openDate DATE , closeDate DATE , clientNote TEXT , managerNote TEXT , priority enum ("Low" , "Medium" , "High") , AssignationStatus enum ("Pending" , "Assigned" , "Reassigned") DEFAULT "Pending" , taskStatus enum ("Pending", "inProgress" , "Completed" , "Closed") DEFAULT "Pending" , clientApproval enum ("Pending" , "Accepted" , "Rejected") DEFAULT "Pending" , managerApproval enum ("Pending" , "Accepted" , "Rejected") DEFAULT "Pending" , taskCategory enum ("Scheduled" , "RunTime") , FOREIGN KEY (clientId) REFERENCES client(clientId) , FOREIGN KEY (operatorId) REFERENCES operator(operatorId) , FOREIGN KEY (managerId) REFERENCES manager(managerId), FOREIGN KEY (projectId) REFERENCES project(projectId));';
 
 let sqlQuery8 =
   "CREATE TABLE IF NOT EXISTS taskTimeline (timelineId VARCHAR(255) PRIMARY KEY, taskId VARCHAR(255), openDate DATE, closeDate DATE, actualCloseDate DATE, managerApprovalDate DATE, clientApprovalDate DATE, managerRejectionDate DATE, clientRejection DATE, assignationDate DATE, completionDate DATE, lastReassignation DATE, operatorAcceptDate DATE, FOREIGN KEY (taskId) REFERENCES task(taskId) );";
@@ -25,12 +25,16 @@ let sqlQuery9 =
 
 let sqlQuery10 =
   'CREATE TABLE IF NOT EXISTS operatorStatus (statusId VARCHAR(255), operatorId VARCHAR(255), taskId VARCHAR(255), operatorNote VARCHAR(255), acceptStatus enum("Pending", "Accepted") DEFAULT "Pending", FOREIGN KEY (operatorId) REFERENCES operator(operatorId), FOREIGN KEY (taskId) REFERENCES task(taskId));';
+
+let sqlQuery11 =
+  "CREATE TABLE IF NOT EXISTS project (projectId VARCHAR(255) PRIMARY KEY, projectName VARCHAR(255), active enum ('Active', 'Deleted') DEFAULT 'Active')";
 module.exports = [
   sqlQuery1,
   sqlQuery2,
   sqlQuery3,
   sqlQuery4,
   sqlQuery5,
+  sqlQuery11,
   sqlQuery6,
   sqlQuery7,
   sqlQuery8,
