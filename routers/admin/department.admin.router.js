18b67703e5b130865a872fbbff41293e342a238b
Rohit Patil <rohitvpatil0810@gmail.com>
2023-03-12 13:46:35 +0530

    restructured departemnet admin router

--- routers/admin/department.admin.router.js
+++ routers/admin/department.admin.router.js
@@ -1,1 +1,21 @@
 const { Router } = require("express");
+const departmentAdminController = require("../../controller/admin/department.admin.controller");
+const departmentAdminRouter = Router();
+
+departmentAdminRouter.post(
+  "/addDepartment",
+  requireAdminAuth,
+  departmentAdminController.addDepartment
+);
+departmentAdminRouter.post(
+  "/editDepartment/:departmentId",
+  requireAdminAuth,
+  departmentAdminController.editDepartment
+);
+departmentAdminRouter.get(
+  "/getDepartments",
+  requireAdminAuth,
+  departmentAdminController.getDepartments
+);
+
+module.exports = departmentAdminRouter;
