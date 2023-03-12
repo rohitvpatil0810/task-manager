8511307771436ee55b08ae38b7e6dc9873840db3
Rohit Patil <rohitvpatil0810@gmail.com>
2023-03-12 15:30:15 +0530

    restructured client admin router

--- routers/admin/admin.router.js
+++ routers/admin/admin.router.js
@@ -1,42 +1,12 @@
 const { Router } = require("express");
 const {
-  createNewAdmin,
   loginAdmin,
   logoutAdmin,
-  createNewManager,
-  createNewClient,
   getAdminProfile,
-  getManagers,
-  getClients,
-  createNewOperator,
-  getOperators,
-  getDepartments,
-  getClientProfilePic,
-  getOperatorProfilePic,
-  deleteOperator,
-  createNewProject,
-  deleteProject,
-  activateProject,
-  getDeactivatedOperators,
-  getProjects,
-  getDeactivatedProjects,
-  getProjectIcon,
-  deleteClient,
-  deleteManager,
-  activateManager,
-  activateOperator,
-  activateClient,
-  getDeactivatedManagers,
-  getDeactivatedClients,
-  editManager,
-  editClient,
-  editOperator,
-  addDepartment,
-  editProject,
-  editDepartment,
 } = require("../../controller/admin/admin.controller");
 const { requireAdminAuth } = require("../../middleware/adminAuth.middleware");
 const reportsRouter = require("../reports/reports.router");
+const clientAdminRouter = require("./client.admin.router");
 const managerAdminRouter = require("./manager.admin.router");
 const operatorAdminRouter = require("./operator.admin.router");
 const projectAdminRouter = require("./project.admin.router");
@@ -47,24 +17,11 @@ adminRouter.use("/reports", requireAdminAuth, reportsRouter);
 adminRouter.use(projectAdminRouter);
 adminRouter.use(operatorAdminRouter);
 adminRouter.use(managerAdminRouter);
-
-adminRouter.get("/getClientProfilePic/:clientId", getClientProfilePic);
-
-adminRouter.post("/addAdmin", requireAdminAuth, createNewAdmin);
-
-adminRouter.post("/addClient", requireAdminAuth, createNewClient);
-adminRouter.get("/deleteClient/:clientId", requireAdminAuth, deleteClient);
-adminRouter.get("/activateClient/:clientId", requireAdminAuth, activateClient);
-adminRouter.get("/getClients", requireAdminAuth, getClients);
-adminRouter.get(
-  "/getDeactivatedClients",
-  requireAdminAuth,
-  getDeactivatedClients
-);
-adminRouter.post("/editClient/:clientId", requireAdminAuth, editClient);
+adminRouter.use(clientAdminRouter);
 
 // admin routes for login and logout
 adminRouter.get("/profile", requireAdminAuth, getAdminProfile);
 adminRouter.post("/login", loginAdmin);
 adminRouter.get("/logout", logoutAdmin);
+
 module.exports = adminRouter;
--- routers/admin/client.admin.router.js
+++ routers/admin/client.admin.router.js
@@ -1,1 +1,46 @@
 const { Router } = require("express");
+const { requireAdminAuth } = require("../../middleware/adminAuth.middleware");
+const clientAdminController = require("../../controller/admin/client.admin.controller");
+const clientAdminRouter = Router();
+
+clientAdminRouter.post(
+  "/addAdmin",
+  requireAdminAuth,
+  clientAdminController.createNewAdmin
+);
+clientAdminRouter.get(
+  "/getClientProfilePic/:clientId",
+  clientAdminController.getClientProfilePic
+);
+clientAdminRouter.post(
+  "/addClient",
+  requireAdminAuth,
+  clientAdminController.createNewClient
+);
+clientAdminRouter.get(
+  "/deleteClient/:clientId",
+  requireAdminAuth,
+  clientAdminController.deleteClient
+);
+clientAdminRouter.get(
+  "/activateClient/:clientId",
+  requireAdminAuth,
+  clientAdminController.activateClient
+);
+clientAdminRouter.get(
+  "/getClients",
+  requireAdminAuth,
+  clientAdminController.getClients
+);
+clientAdminRouter.get(
+  "/getDeactivatedClients",
+  requireAdminAuth,
+  clientAdminController.getDeactivatedClients
+);
+clientAdminRouter.post(
+  "/editClient/:clientId",
+  requireAdminAuth,
+  clientAdminController.editClient
+);
+
+module.exports = clientAdminRouter;
